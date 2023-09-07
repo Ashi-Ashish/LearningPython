d5c811026c6b87ab375c53ba65380b80a1b61ceb
AshishRalh <46755420+AshishRalh@users.noreply.github.com>
2023-09-07 10:49:09 -0400

    Trees

---
+++ Graphs/Graphs.py
@@ -0,0 +1,146 @@
+import numpy as np
+from Queue.QueuesLinked import QueuesLinked
+
+class Graph:
+    def __init__(self, vertices):
+        self._vertices = vertices
+        self._adjMat = np.zeros((vertices, vertices))
+        self._visited = [0] * self._vertices
+
+    def insert_edge(self, u, v, x=1):
+        self._adjMat[u][v] = x
+
+    def remove_edge(self, u, v):
+        self._adjMat[u][v] = 0
+
+    def exist_edge(self, u, v):
+        return self._adjMat[u][v] != 0
+
+    def vertex_count(self):
+        return self._vertices
+
+    def edge_count(self):
+        count = 0
+        for i in range(self._vertices):
+            for j in range(self._vertices):
+                if self._adjMat[i][j] != 0:
+                    count += 1
+
+        return count
+
+    def vertices(self):
+        for i in range(self._vertices):
+            print(i, end=' ')
+        print()
+
+    def edges(self):
+        for i in range(self._vertices):
+            for j in range(self._vertices):
+                if self._adjMat[i][j] != 0:
+                    print(i, '--', j)
+
+    def outdegree(self, v):
+        count = 0
+        for j in range(self._vertices):
+            if self._adjMat[v][j] != 0:
+                count += 1
+        return count
+
+    def indegree(self, v):
+        count = 0
+        for i in range(self._vertices):
+            if self._adjMat[i][v] != 0:
+                count += 1
+        return count
+
+    def BFS(self, s):
+        i = s
+        q = QueuesLinked()
+        visited = [0] * self._vertices
+        print(i, end=' ')
+        visited[i] = 1
+        q.enqueue(i)
+        while not q.isempty():
+            i = q.dequeue()
+            for j in range(self._vertices):
+                if self._adjMat[i][j] == 1 and visited[j] == 0:
+                    print(j, end=' ')
+                    visited[j] = 1
+                    q.enqueue(j)
+
+    def DFS(self, s):
+        if self._visited[s] == 0:
+            print(s, end=' ')
+            self._visited[s] = 1
+
+            for j in range(self._vertices):
+                if self._adjMat[s][j] == 1 and self._visited[j] == 0:
+                    self.DFS(j)
+
+
+
+    def display_adjMat(self):
+        print((self._adjMat))
+
+# Undirected Graph
+
+# G = Graph(4)
+# G.display_adjMat()
+# print('Vertices:', G.vertex_count())
+# print('Edges:', G.edge_count())
+# G.insert_edge(0, 1, 26)
+# G.insert_edge(0, 2, 16)
+# G.insert_edge(1, 0, 26)
+# G.insert_edge(1, 2, 12)
+# G.insert_edge(2, 0, 16)
+# G.insert_edge(2, 1, 12)
+# G.insert_edge(2, 3, 8)
+# G.insert_edge(3, 2, 8)
+# G.display_adjMat()
+# print('Edges:', G.edge_count())
+# G.edges()
+# print('Edge between 1-3', G.exist_edge(1,3))
+# print('Edge between 1-2', G.exist_edge(1,2))
+# print('Degree', G.indegree(2))
+# G.remove_edge(1, 2)
+# print('Edge between 1-2', G.exist_edge(1,2))
+# print('Degree', G.indegree(2))
+
+
+G = Graph(7)
+G.insert_edge(0, 1)
+G.insert_edge(0, 5)
+G.insert_edge(0, 6)
+G.insert_edge(1, 0)
+G.insert_edge(1, 2)
+G.insert_edge(1, 5)
+G.insert_edge(1, 6)
+G.insert_edge(2, 3)
+G.insert_edge(2, 4)
+G.insert_edge(2, 6)
+G.insert_edge(3, 4)
+G.insert_edge(4, 2)
+G.insert_edge(4, 5)
+G.insert_edge(5, 2)
+G.insert_edge(5, 3)
+G.insert_edge(6, 3)
+G.display_adjMat()
+print('DFS')
+G.DFS(1)
+
+
+
+
+#
+# print('Vertices:', G.vertex_count())
+# print('Edges:', G.edge_count())
+# G.display_adjMat()
+# print('Edges:', G.edge_count())
+# G.edges()
+# print('Edge between 1-3', G.exist_edge(1,3))
+# print('Edge between 1-2', G.exist_edge(1,2))
+# print('Indegree', G.indegree(2))
+# print('outdegree', G.outdegree(2))
+# G.remove_edge(1, 2)
+# print('Edge between 1-2', G.exist_edge(1,2))
+# print('Indegree', G.indegree(2))
